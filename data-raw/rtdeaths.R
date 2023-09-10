b351c3a9bc27f81360355b28b6a7499b1510a89f
pabsantos <pabsantos21@gmail.com>
2023-09-10 13:55:11 -0300

    Download datasus files

--- data-raw/rtdeaths.R
+++ data-raw/rtdeaths.R
@@ -3,5 +3,10 @@
 library(microdatasus)
 library(tidyverse)
 
+datasus_doext <- fetch_datasus(
+  year_start = 1996,
+  year_end = 2021,
+  information_system = "SIM-DOEXT"
+)
 
 usethis::use_data(rtdeaths, overwrite = TRUE)
